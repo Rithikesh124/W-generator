365baf307123437212f12979f3c9afb029447738
Rithikesh124 <141357919+Rithikesh124@users.noreply.github.com>
2026-01-04 20:58:49 +0530

    Fix response key from 'phrase' to 'words'

--- api/generate.js
+++ api/generate.js
@@ -31,7 +31,7 @@ export default async function handler(req) {
     }
 
     return new Response(JSON.stringify({ 
-      phrase: phrase.join("")
+      words: phrase.join("")
     }), {
       status: 200,
       headers: { 
